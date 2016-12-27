4f6f0e25ff4fea4dc38d3be87b58bad4b0983cf5
Yoscroll <ianhenry.c808@gmail.com>
2016-12-26 17:14:06 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 var fortunesList = ["you are rich","you got a free trip to hawaii","try again","make a wish!","you won free tickets"];
-function generateFortuneCookie(argument) {
-  var box = document.getElementById('fortune-cookie-text');
-  var list = document.getElementById('list');
-  var li = document.createElement("li");
-  box.innerHTML = fortunesList[Math.floor(Math.random()*fortunesList.length)];
-  li.appendChild(document.createTextNode(box.textContent));
-  list.appendChild(li);
+var fortune=null;
+function generateFortuneCookie() {
+	var box = document.getElementById('fortune-cookie-text');
+	var list = document.getElementById('list');
+	var li = document.createElement("li");
+	if (fortune!==null){
+		li.appendChild(document.createTextNode(fortune));
+		list.appendChild(li);
+	}	
+	fortune = fortunesList[Math.floor(Math.random()*fortunesList.length)];
+	box.innerHTML = fortune;
 }
