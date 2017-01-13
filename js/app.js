fa14cdd8a4700e1f16d44c263c418534c76a8fbb
Yoscroll <ianhenry.c808@gmail.com>
2017-01-13 08:35:40 -1000

    shorter code

--- js/app.js
+++ js/app.js
@@ -1,31 +1,36 @@
-var fortunesList = ["you're rich","you're poor","you're going to ace all your classes","you have infinite cash"];
-var fortunesToSay=[];
-for (var i = 0; i < fortunesList.length; i++) {
-		fortunesToSay.push(fortunesList[i]);
-	}
-var fortune=null;
+var fortunesToSay = ["you're rich","you're poor","you're going to ace all your classes","you have infinite cash","a potato will come flying on your front door"];
+var fortune="";
+var saidAll=false;
 
 function generateFortuneCookie() {
-	document.getElementById('onlyButton').innerHTML="Click Again!";
-	var box = document.getElementById('fortune-cookie-text');
-	var list = document.getElementById('list');
-	var li = document.createElement("li");
-	if (fortune!==null&&fortunesToSay.length>0){
-		li.appendChild(document.createTextNode(fortune));
-		list.appendChild(li);
-	}
-	var num;
-	if (fortunesToSay.length>0) {
-		num =Math.floor(Math.random()*fortunesToSay.length);
-		fortune = fortunesToSay[num];
-		var index = fortunesToSay.indexOf(fortune);
-		if (index > -1) {
-	    	fortunesToSay.splice(index, 1);
-	    	box.innerHTML = "<b>Fortunes</b> <br><br>"+fortune;
+	if(saidAll==false){
+		//stuff	
+		var btn= document.getElementById('onlyButton')
+		var box = document.getElementById('fortune-cookie-text');
+		var list = document.getElementById('list');
+		var li = document.createElement("li");
+		btn.innerHTML="Click Again!";
+
+		if (fortune!=""){
+			//adds current fortune to the previous fortunes list
+			li.appendChild(document.createTextNode(fortune));
+			list.appendChild(li);
 		}
-	}
-	else{
-		document.getElementById('onlyButton').innerHTML="You ran out of fortunes!";
-		box.innerHTML = "<b>Fortunes</b> <br><br> You ran out of fortunes!";
+		if (fortunesToSay.length>0) {
+			//find a random fortune from the fortunesToSay
+			var num;
+			num =Math.floor(Math.random()*fortunesToSay.length);
+			fortune = fortunesToSay[num];
+
+			//find the fortune and deletes it from fortunesToSay
+		  fortunesToSay.splice(num, 1);
+		  box.innerHTML = "<b>Fortunes</b> <br><br>"+fortune;
+		}
+		else{
+			//notfies that there aren't any fortunes left
+			btn.innerHTML="You ran out of fortunes!";
+			box.innerHTML = "<b>Fortunes</b> <br><br> You ran out of fortunes!";
+			saidAll=true;
+		}
 	}
 }
